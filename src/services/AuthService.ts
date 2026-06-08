e7d2d069b0cb3865bebf1334b4d86dd4f9cbb68d
Gulzar <gulzarhuseynovaa@gmail.com>
2026-06-08 16:35:49 +0400

    Add vercel.json to fix login API rewrit3

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -3,22 +3,18 @@ import type { RegisterForm } from "../types/Register.type";
 import type { LoginForm } from "../types/Login.type";
 import type { VerifyForm } from "../types/Verify.type";
 
-const API_URL = "/intern-api/api";
-
-
-
 export const AuthService = {
   register: (data: RegisterForm) => {
-    return api.post(`${API_URL}/auth/register`, data);
+    // API_URL hissəsini sildik
+    return api.post(`/auth/register`, data);
   },
   verifyEmail: (data: VerifyForm) => {
-    return api.post(`${API_URL}/auth/verify-email`, data);
+    return api.post(`/auth/verify-email`, data);
   },
   login: (data: LoginForm) => {
-    return api.post(`${API_URL}/auth/login`, data);
+    return api.post(`/auth/login`, data);
   },
   getProfile: () => {
     return api.get("/auth/profile");
   },
-
-};
+}
